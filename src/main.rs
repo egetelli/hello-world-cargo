7a8b2e51a00dc206057f57381494ddcc6e6b4ff2
egetelli <egetelli@hotmail.com>
2022-08-09 15:19:13 +0300

    düzenleme

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,57 @@
 fn main() {
     println!("Hello, world!");
     let mut x = 45;
-    println!("The value of x is {}",x);
+    println!("The value of x is {}", x);
     let mut y = 60;
-    println!("The value of x is {}",x);
+    println!("The value of x is {}", x);
     let mut f = 5.5;
-    println!("The value of f is {}",f);
+    println!("The value of f is {}", f);
 
     let ege: f32 = 10.5;
-    println!("The value of ege is {}",ege);
-
+    println!("The value of ege is {}", ege);
 
     let a = false;
-    println!("The value of a is {}",a);
+    println!("The value of a is {}", a);
 
-
-    if x>y{
+    if x > y {
         println!("x, y'den büyük");
-    }
-    else{
+    } else {
         println!("y, x'den büyük");
     }
 
     let mut a = 0;
 
-    loop{
-        a+=3;
+    loop {
+        a += 3;
 
-        if a > 100{
+        if a > 100 {
             break;
         }
-        println!("Sayımız = {}",a);
+        println!("Sayımız = {}", a);
     }
     let mut b = 0;
-    while b <=100{
-        
-        if b%10==0{
-            println!("Bu Sayılar 10'un Katı : {}",b);
+    while b <= 100 {
+        if b % 10 == 0 {
+            println!("Bu Sayılar 10'un Katı : {}", b);
         }
-        
 
-        b = b+1;
-
+        b = b + 1;
     }
 
-    for i in 1..30{
-        if i%5 == 0{
-            println!("Bu sayılar 5'in katı {}",i);
+    for i in 1..30 {
+        if i % 5 == 0 {
+            println!("Bu sayılar 5'in katı {}", i);
         }
     }
 
-    let animals = vec!["Dog","Rabbit","Cat","Turtle","Zebra"];
-    for (index,a) in animals.iter().enumerate(){
-        println!("{} Numaralı hayvanın ismi {}",index,a);
+    let animals = vec!["Dog", "Rabbit", "Cat", "Turtle", "Zebra"];
+    for (index, a) in animals.iter().enumerate() {
+        println!("{} Numaralı hayvanın ismi {}", index, a);
     }
 
+    let suankigun: Gunler = Gunler::Sali;
 
-
-
-    let suankigun:Gunler = Gunler::Sali;
-
-    
-
-    match suankigun{
+    match suankigun {
         Gunler::Pazartesi => println!("Günümüz Pazartesi"),
         Gunler::Sali => println!("Günümüz sali"),
         Gunler::Carsamba => println!("Günümüz carsamba"),
@@ -72,76 +61,97 @@ fn main() {
         Gunler::Pazar => println!("Günümüz pazar"),
     }
 
+    //     let mut line = String::new();
+    //    println!("Enter your name :");
+    //    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    //    print!("Hello , {}", line);
+    //    println!("no of bytes read , {}", b1);
 
-//     let mut line = String::new();
-//    println!("Enter your name :");
-//    let b1 = std::io::stdin().read_line(&mut line).unwrap();
-//    print!("Hello , {}", line);
-//    println!("no of bytes read , {}", b1);
+    //******************************************* STRING ÖRNEKLERİ
 
-    let tup = (15,"ege",25.5,false,"e");
+    let tup = (15, "ege", 25.5, false, "e");
 
-    let (a,b,c,d,e) = tup;
+    let (a, b, c, d, e) = tup;
 
-    println!("a is {}",a);
-    println!("b is {}",b);
-    println!("c is {}",c);
-    println!("d is {}",d);
-    println!("e is {}",e);
+    println!("a is {}", a);
+    println!("b is {}", b);
+    println!("c is {}", c);
+    println!("d is {}", d);
+    println!("e is {}", e);
 
-    
     print_numbers_to(50);
 
+    let fullname = " Tutorials Point \r\n";
+    println!("Before trim {}", fullname);
 
+    println!("length is {}", fullname.len());
+    println!();
+    println!("After trim  {}", fullname);
+    println!("length is {}", fullname.trim().len());
+    println!("After trim  {}", fullname);
 
-     let fullname = " Tutorials Point \r\n";
-     println!("Before trim {}",fullname);
+    let msg = "Tutorials Point has good tutorials".to_string();
+    let mut i = 1;
 
-     println!("length is {}",fullname.len());
-     println!();
-     println!("After trim  {}",fullname);
-     println!("length is {}",fullname.trim().len());
-     println!("After trim  {}",fullname);
+    for token in msg.split_whitespace() {
+        println!("token {} {}", i, token);
+        i += 1;
+    }
 
+    let fullname = "Kannan,Sudhakaran,Tutorialspoint";
 
-     let msg = "Tutorials Point has good tutorials".to_string();
-     let mut i = 1;
-     
-     for token in msg.split_whitespace(){
-        println!("token {} {}",i,token);
-        i+=1;
-     }
+    for token in fullname.split(",") {
+        println!("token is {}", token);
+    }
 
+    //store in a Vector
+    println!("\n");
+    let tokens: Vec<&str> = fullname.split(",").collect();
+    println!("firstName is {}", tokens[0]);
+    println!("lastname is {}", tokens[1]);
+    println!("company is {}", tokens[2]);
+
+    let n1 = "Tutorials".to_string();
+    let n2 = "Point".to_string();
+ 
+    let n3 = n1 + &n2; // n2 reference is passed
+    println!("{}",n3);
+
+    let n1 = "Tutorials".to_string();
+    let n2 = "Point".to_string();
+    let n3 = format!("{} {}",n1,n2);
+    println!("{}",n3);
+
+    //******************************************* STRING ÖRNEKLERİ
+
+    
 
 }
 
 
 
-fn print_numbers_to(num: u32){
-    for c in 1..num{
-        if is_even(c){
-            println!("{} is even",c);
-        }
-        else{
-            println!("{} is odd",c);
+
+
+fn print_numbers_to(num: u32) {
+    for c in 1..num {
+        if is_even(c) {
+            println!("{} is even", c);
+        } else {
+            println!("{} is odd", c);
         }
     }
 }
 
-
 fn is_even(num: u32) -> bool {
-    return num%2==0;
+    return num % 2 == 0;
 }
 
-
-enum Gunler{
-
+enum Gunler {
     Pazartesi,
     Sali,
     Carsamba,
     Persembe,
     Cuma,
     Cumartesi,
-    Pazar
-
+    Pazar,
 }
